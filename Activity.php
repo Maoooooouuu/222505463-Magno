ef2e170aca7ff95737b8448c709aabe4a0ed4e39
unknown <markmagno258@gmail.com>
2024-10-09 23:31:22 +0800

    finalizing the activity.php

--- Activity.php
+++ Activity.php
@@ -18,25 +18,34 @@
     ?>
     <h1>Activity 2</h1>
     <?php
-    $password = isset($post['password']) ? $post['password'] : '';
+$correct_password = "password123";
 
-    do {
-        if ($password !== "password123"){
-            echo "<h2>Invalid password. Please try again.<h2>";
-            echo "<form method ="post">
-            <label for="password"> Password:</label>
-            <input type="password" id="password" name="password">
-            <input type="submit" value="submit">
-            </form>";
-        }
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $input_password = $_POST['password'];
     
+    if ($input_password === $correct_password) {
+        echo "Access Granted.";
+    } else {
+        echo "Incorrect password.";
     }
-    while ($password !== "password123");
-
-    if ($password === "password123") {
-        echo "<h2>Access Granted.<h2>";
-    }
+}
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Password Validator</title>
+</head>
+<body>
+    <form method="post" action="">
+        <label>Please enter the password:</label>
+        <input type="password" name="password">
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
+
+
 <h1>Activity 3</h1>
 <?php
 for ($i = 1; $i <=10; $i++){
@@ -53,13 +62,13 @@ for ($i = 1; $i <= 10; $i++){
         continue;
     }
     if ($i == 9){
-        break
+        break;
     }
     echo $i . " ";
 }
 ?>
-<?php
 <h1>activity 5</h1>
+<?php
 $sum = 0;
 $num = 1;
 
@@ -67,7 +76,7 @@ while ($num <= 100) {
     $sum += $num;
     $num++;
 }
-echo "the sum of the numbers from 1 to 100 is: " . $ sum;
+echo "the sum of the numbers from 1 to 100 is: " . $sum;
 ?>
 <h1>activity 6</h1>
 
@@ -76,7 +85,9 @@ $mov = ["The Shawshank Redemption", "Inception", "The Dark Knight", "Interstella
 
 $counter = 1;
 
-foreach "$mov as $mov<br>";
+foreach ($mov as $key => $value) {
+    echo "$key => $value<br>";
+}
 $counter++;
 ?>
 <h1>activity 7</h1>
@@ -92,7 +103,101 @@ $stud = [
 foreach ($stud as $key => $value){
     echo "$key: $value<br>";
 }
+?>
+<h1>Activity 8</h1>
+    <?php
+    $num = 5;
+    $fac = 1;
 
+    for ($i = $num; $i >= 1; $i--){
+        $fac*=$i;
+    }
+    echo "the factorial of $num is: $fac";
+    ?>
+
+    <h1>Activity 9</h1>
+    <?php
+    for ($i = 1; $i <= 50; $i++){
+        if ($i % 3 == 0 && $i % 5 == 0){
+            echo "FizzBuzz ";
+        }
+        elseif ($i % 3 == 0){
+            echo "Fizz ";
+        }
+        elseif ($i % 5 == 0){
+            echo "Buzz ";
+        }
+        else{
+            echo $i . " ";
+        }
+    }
+    ?>
+
+    <h1>Activity 10</h1>
+    <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['number'])) {
+    $number = $_POST['number'];
+    $is_prime = true;
+
+    if ($number < 2) {
+        $is_prime = false;
+    } else {
+        for ($i = 2; $i <= sqrt($number); $i++) {
+            if ($number % $i == 0) {
+                $is_prime = false;
+                break;
+            }
+        }
+    }
+
+    if ($is_prime) {
+        echo "$number is a prime number.";
+    } else {
+        echo "$number is not a prime number.";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Prime Number Checker</title>
+</head>
+<body>
+    <form method="post" action="">
+        <label>Enter a number:</label>
+        <input type="number" name="number">
+        <input type="submit" value="Check">
+    </form>
+</body>
+</html>
+
+
+    <h1>Activity 11</h1>
+    <?php
+    $first = 0;
+    $second = 1;
+    $count = 0;
+    $limit = 10;
+
+    while ($count < $limit){
+        echo $first . " ";
+
+        $next = $first + $second;
+
+        $first = $second;
+        $second = $next;
+
+        $count++;
+    }
+    ?>
+
+    <h1>Activity 12</h1>
+    <?php
+      $reversi = readline('Input something: ');
+      
+      echo "output:" .strrev ($reversi);
+    ?>
 
 </body>
 </html>
